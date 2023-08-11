94cb588be686f3f66e85d0e5ccf31f44ea5b268d
LORENZO-BOTJJ <lorenzoqeladoqqq@gmail.com>
2023-08-11 19:42:03 +0700

    REST APIS

---
+++ controllers/settings.js
@@ -0,0 +1,13 @@
+global.author = "LORENZO";
+global.key_free = ["freeapikey"];
+global.key_premium = [lorenzo];
+global.limit_free = "30";
+global.limit_premium = "99999";
+global.dateLimit = "Oktober, 31 2022",
+  global.mongo_Db = "mongodb+srv://daniyasha28:Lorenzo27@cluster0.q9r0smk.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
+global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
+global.my_email = "chanzxcoderid@gmail.com";
+global.my_email_password = "fjakkuncqitohmzv";
+global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
+global.port = 8080
