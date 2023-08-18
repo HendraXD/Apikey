0bbf7744038601fee1c375e7c3a9b93d56739fbb
HendraXD <142194000+HendraXD@users.noreply.github.com>
2023-08-18 15:19:45 +0700

    Update settings.js

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,13 +1,12 @@
-global.author = "LORENZO";
-global.key_free = ["ytlorenzobotmd"];
-global.key_premium = ["lorenzo"];
+global.author = "HxcoderID";
+global.key_free = ["HxcoderID"];
+global.key_premium = ["HxcoderID130701"];
 global.limit_free = "30";
 global.limit_premium = "99999";
-global.dateLimit = "Oktober, 31 2022",
-  global.mongo_Db = "mongodb+srv://daniyasha28:Lorenzo27@cluster0.q9r0smk.mongodb.net/?retryWrites=true&w=majority";
-global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
-global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
-global.my_email = "chanzxcoderid@gmail.com";
-global.my_email_password = "fjakkuncqitohmzv";
+global.mongo_Db = "mongodb+srv://hendraxd:hFPdksuMrRxugncf@hendraxd.oamahep.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LdPjp0nAAAAACJ_oFFvYFy3XCdAlJ5xXoHznsue";
+global.recaptcha_key_2 = "6LdPjp0nAAAAAA-IuS8hH3tZrnoKn3V7KgFEOhMt";
+global.my_email = "hgindonesia3@gmail.com";
+global.my_email_password = "ufbdqtihvtitilqa";
 global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
 global.port = 8080
